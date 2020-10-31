31246022c0c6976e242585fd86e73412b0f5472d
KushRohra <iec2017024@iiita.ac.in>
2020-11-01 00:04:43 +0530

    4th set of ques

---
+++ Basic_DSA_Codes/19. Validate BST.py
@@ -0,0 +1,10 @@
+# O(n) time | O(d) space where d is the height of the tree 
+def validateBST(tree):
+	return validateBSTHelper(tree, float("-inf"), float("inf"))
+
+def validateBSTHelper(tree, minValue, maxValue):
+	if tree is None:
+		return True
+	if tree.value < minValue or tree.value >= maxValue:
+		return False
+	return validateBSTHelper(tree.left, minValue, tree.value) and validateBSTHelper(tree.right, tree.value, maxValue)
