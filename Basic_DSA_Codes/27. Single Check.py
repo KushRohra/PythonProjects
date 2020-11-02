39e06d2e2d25c383287530cfcb802429a91fcab8
KushRohra <iec2017024@iiita.ac.in>
2020-11-03 00:01:04 +0530

    6th set of codes

---
+++ Basic_DSA_Codes/27. Single Check.py
@@ -0,0 +1,15 @@
+# O(n) time | O(1) space
+def hasSingleCycle(array):
+	numElementsVisited = 0
+	currentIndex = 0
+	while numElementsVisited < len(array):
+		if numElementsVisited > 0 and currentIndex == 0:
+			return False
+		numElementsVisited += 1
+		currentIndex = getNextIndex(currentIndex, array)
+	return currentIndex == 0
+
+def getNextIndex(currentIndex, array):
+	jump = array[currentIndex]
+	nextIndex = (currentIndex + jump) % len(array)
+	return nextIndex if nextIndex >=0 else nextIndex + len(array)
---
+++ Basic_DSA_Codes/29. River Sizes.py
@@ -0,0 +1,37 @@
+def riverSizes(matrix):
+	sizes = []
+	visited = [[False for value in row] for row in matrix]
+	for i in range(len(matrix)):
+		for j in range(len(matrix[i])):
+			if visited[i][j] == True:
+				continue
+			traverseNode(i, j, matrix, visited, sizes)
+
+def traverseNode(i, j, matrix, visited, sizes):
+	currentRiverSize = 0
+	nodesToExplore = [[i, j]]
+	while len(nodesToExplore):
+		i, j = nodesToExplore.pop()
+		if visited[i][j]:
+			continue
+		visited[i][j] = True
+		if matrix[i][j] == 0:
+			continue
+		currentRiverSize += 1
+		unvisitedNeighbors = getUnvisitedNeighbors(i, j, matrix, visited)
+		for neighbor in unvisitedNeighbors:
+			nodesToExplore.append(neighbor)
+
+	if currentRiverSize > 0:
+		sizes.append(currentRiverSize)
+
+def getUnvisitedNeighbors(i, j, matrix, visited):
+	nodesToReturn = []
+	X = [-1, 0, 0, 1]
+	Y = [0, 1, -1, 0]
+	for k in range(4):
+		newX = i + X[k]
+		newY = j + Y[k]
+		if newX >=0 and newX < len(matrix) and newY >= 0 and newY < len(matrix[0]) and matrix[newX][newY] == 1 and visited[newX][newY] == False:
+			nodesToReturn.append([newX, newY])
+	return nodesToReturn
