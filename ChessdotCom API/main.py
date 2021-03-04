e9e186e58099489880b3dcc2f00010c0685172e6
Kush <kushrohra13@gmail.com>
2021-03-04 11:19:51 +0530

    print leaderboard function added

--- ChessdotCom API/main.py
+++ ChessdotCom API/main.py
@@ -3,29 +3,43 @@ import pprint
 
 printer = pprint.PrettyPrinter()
 
-while True:
-    print("1. Print the leaderboards")
-    print("2. Get stats for a player")
-    print("3. Get recent game details for a player")
-    print("4. To exit the process")
-    choice = input("Enter a choice number between 1 and 4 ")
-    try:
-        choice = int(choice)
-    except Exception as e:
-        print("Please enter a valid number choice")
-    if choice == 1:
-        print()
-        print_leaderboards()
-        print()
-    elif choice == 2:
-        print()
-        username = input("Enter the username for the player you want to get the stats for: ")
-        get_player_ratings(username)
-        print()
-    elif choice == 3:
-        print()
-        username = input("Enter the username for the player you want to get recent game details for: ")
-        get_most_recent_game(username)
-        print()
-    elif choice == 4:
-        break
+
+def print_leaderboards():
+    data = get_leaderboards().json
+    categories = data.keys()
+
+    for category in categories:
+        print("Category : ", category)
+        for index, entry in enumerate(data[category]):
+            print(f'Rank: {index + 1} | Username: {entry["username"]} | Rating: {entry["score"]}')
+
+def main():
+    while True:
+        print("1. Print the leaderboards")
+        print("2. Get stats for a player")
+        print("3. Get recent game details for a player")
+        print("4. To exit the process")
+        choice = input("Enter a choice number between 1 and 4 ")
+        try:
+            choice = int(choice)
+        except Exception as e:
+            print("Please enter a valid number choice")
+        if choice == 1:
+            print()
+            print_leaderboards()
+            print()
+        elif choice == 2:
+            print()
+            username = input("Enter the username for the player you want to get the stats for: ")
+            get_player_ratings(username)
+            print()
+        elif choice == 3:
+            print()
+            username = input("Enter the username for the player you want to get recent game details for: ")
+            get_most_recent_game(username)
+            print()
+        elif choice == 4:
+            break
+
+if __name__ == "__main__":
+    main()
