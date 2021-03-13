5dd6a01282d2d315029b4daf85c26dd2be6fb806
Kush <kushrohra13@gmail.com>
2021-03-13 14:12:02 +0530

    convert running time string to integers

--- Data Science/Movie Dataset Creation/scrape.py
+++ Data Science/Movie Dataset Creation/scrape.py
@@ -6,13 +6,23 @@ import json
 def get_content_value(row_data):
     if row_data.find("li"):
         return [str(li.get_text(" ", strip=True)).replace("\xa0", " ") for li in row_data.find_all("li")]
+    elif row_data.find("br"):
+        return [text for text in row_data.stripped_strings]
     else:
         return str(row_data.get_text(" ", strip=True)).replace("\xa0", " ")
+
+
+def clean_tags(soup):
+    # removes references and dates formatted in brackets
+    for tag in soup.find_all(["sup", "span"]):
+        tag.decompose()
 
 
 def get_movie_details(movie_url):
     r = requests.get(movie_url)
     soup = bs(r.content, features='lxml')
+
+    clean_tags(soup)
 
     movie_info = {}
 
@@ -21,35 +31,71 @@ def get_movie_details(movie_url):
     for index, row in enumerate(info_rows):
         if index == 0:
             movie_info['title'] = row.find("th").get_text(" ", strip=True)
-        elif index == 1:
-            continue
         else:
-            content_key = str(row.find("th").get_text(" ", strip=True))
-            content_value = get_content_value(row.find("td"))
-            movie_info.update({content_key: content_value})
+            header = row.find('th')
+            if header:
+                content_key = str(row.find("th").get_text(" ", strip=True))
+                content_value = get_content_value(row.find("td"))
+                movie_info.update({content_key: content_value})
 
     return movie_info
 
 
-if __name__ == "__main__":
+def minute_to_integer():
+    with open('movie_data.json', 'r', encoding='utf-8') as f:
+        movie_list = json.load(f)
+
+    for movie in movie_list:
+        time = movie.get('Running time', 'NA')
+        if type(time) is list:
+            time = time[0]
+        time = time.split(' ')[0]
+        try:
+            del movie['Running time']
+        except Exception as e:
+            pass
+        movie.update({'Running time (int)': time})
+
+    with open('movie_data.json', 'w', encoding='utf-8') as f:
+        json.dump(movie_list, f, ensure_ascii=False, indent=2)
+
+
+def clean_data():
+    # Cleanup references => remove [1], [2]
+    # Split up the long strings
+
+    # Convert running time into integers
+    minute_to_integer()
+
+    # Convert dates into datetime objects
+    # Convert budget and box office to numbers
+    pass
+
+
+def main():
     r = requests.get("https://en.wikipedia.org/wiki/List_of_Walt_Disney_Pictures_films")
     soup = bs(r.content, features='lxml')
 
     base_path = "https://en.wikipedia.org/"
     movie_info_list = []
-    movie_info_errors = []
 
     movies = soup.select(".wikitable.sortable i a")
     for index, movie in enumerate(movies):
+        if index == 41:
+            break
+        if index % 10 == 0:
+            print(index)
         try:
             full_path = base_path + movie['href']
             movie_name = movie.get_text()
             movie_info_list.append(get_movie_details(full_path))
         except Exception as e:
-            movie_info_errors.append({'movie_name': movie.get_text()})
+            continue
 
-    with open('movie_data.json', encoding='utf-8') as f:
+    with open('movie_data.json', 'w', encoding='utf-8') as f:
         json.dump(movie_info_list, f, ensure_ascii=False, indent=2)
 
-    with open('movie_info_error.json', encoding='utf-8') as f:
-        json.dump(movie_info_errors, f, ensure_ascii=False, indent=2)
+
+if __name__ == "__main__":
+    # main()
+    clean_data()
