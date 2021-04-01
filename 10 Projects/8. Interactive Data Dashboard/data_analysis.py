aabaeb2cf12da089caa3db01fd6990586032c122
KushRohra <kushrohra13@gmail.com>
2021-04-01 10:37:27 +0530

    Styling the chart and making it responsive

--- 10 Projects/8. Interactive Data Dashboard/data_analysis.py
+++ 10 Projects/8. Interactive Data Dashboard/data_analysis.py
@@ -13,7 +13,7 @@ def inc_dec(c, o):
         return "Equal"
 
 
-start = datetime(2016, 3, 1)
+start = datetime(2015, 11, 1)
 end = datetime(2016, 3, 10)
 
 df = data.DataReader(name="GOOG", data_source="yahoo", start=start, end=end)
@@ -21,10 +21,11 @@ df["Status"] = [inc_dec(c, o) for c, o in zip(df.Close, df.Open)]
 df["Middle"] = (df.Close + df.Open) / 2
 df["Height"] = abs(df.Close - df.Open)
 
-p = figure(x_axis_type='datetime', width=1000, height=300)
+p = figure(x_axis_type='datetime', width=1000, height=300, sizing_mode="scale_width")
 t = Title()
 t.text = "Candlestick Chart"
 p.title = t
+p.grid.grid_line_alpha = 0.5
 
 hours_12 = 12 * 60 * 60 * 1000  # 12 hrs into ms
 
@@ -39,7 +40,12 @@ p.rect(x_close_more, y_centre_point, hours_12, width, fill_color='green', line_c
 p.rect(x_close_less, y_centre_point, hours_12, width, fill_color='red', line_color='black')'''
 
 # Method 2 : Using an additional field
-p.rect(df.index[df.Status == "Increase"], df.Middle[df.Status == "Increase"], hours_12, df.Height[df.Status == "Increase"], fill_color='green', line_color='black')
-p.rect(df.index[df.Status == "Decrease"], df.Middle[df.Status == "Decrease"], hours_12, df.Height[df.Status == "Decrease"], fill_color='red', line_color='black')
+p.segment(df.index, df.High, df.index, df.Low, color='black')
+
+p.rect(df.index[df.Status == "Increase"], df.Middle[df.Status == "Increase"], hours_12,
+       df.Height[df.Status == "Increase"], fill_color='#CCFFFF', line_color='black')
+p.rect(df.index[df.Status == "Decrease"], df.Middle[df.Status == "Decrease"], hours_12,
+       df.Height[df.Status == "Decrease"], fill_color='#FF3333', line_color='black')
+
 output_file("CS.html")
 show(p)
