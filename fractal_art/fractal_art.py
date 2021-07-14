8e47dbe875e10d544f8a8668f144a084675886bf
Kush Rohra <kushrohra13@gmail.com>
2021-07-14 12:10:49 +0530

    adding fractal art

---
+++ fractal_art/fractal_art.py
@@ -0,0 +1,48 @@
+from turtle import *
+
+shape("turtle")
+speed(0)
+
+
+def tree(size, levels, angle):
+    if levels == 0:
+        color("green")
+        dot(size)
+        color("black")
+        return
+
+    color("brown")
+    forward(size)
+    right(angle)
+    tree(size * 0.8, levels - 1, angle)
+    left(angle * 2)
+    tree(size * 0.8, levels - 1, angle)
+    right(angle)
+    backward(size)
+
+
+def snowflake_side(length, levels):
+    if levels == 0:
+        forward(length)
+        return
+
+    length /= 3.0
+    snowflake_side(length, levels - 1)
+    left(60)
+    snowflake_side(length, levels - 1)
+    right(120)
+    snowflake_side(length, levels - 1)
+    left(60)
+    snowflake_side(length, levels - 1)
+
+
+def create_snowflake(length, sides):
+    for _ in range(sides):
+        snowflake_side(length, sides)
+        right(360 / sides)
+
+
+create_snowflake(200, 4)
+left(90)
+tree(70, 7, 30)
+mainloop()
